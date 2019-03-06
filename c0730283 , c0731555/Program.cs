4af7b6aef044eb96c82056173db1d223ac9093fa
visualstudio <visualstudio@122-PC25>
2019-03-06 10:18:58 -0500

    Final Lab01

--- c0730283 , c0731555/Program.cs
+++ c0730283 , c0731555/Program.cs
@@ -11,13 +11,18 @@ namespace c0730283___c0731555
 {
     public class DelegateExercises
     {
+        //student name navita navita
+        //studentname gagandeep sidhu
         public delegate void MyDelegate();
         void Method1()
         {
             Console.WriteLine("Method!");
             Console.ReadLine();
         }
-        
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
 
     }
