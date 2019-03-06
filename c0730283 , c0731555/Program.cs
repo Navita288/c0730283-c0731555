fa42ccf7edd832ec73fe71977179259a4c8989e1
visualstudio <visualstudio@122-PC25>
2019-03-06 10:58:45 -0500

    lab4

--- c0730283 , c0731555/Program.cs
+++ c0730283 , c0731555/Program.cs
@@ -13,17 +13,17 @@ namespace c0730283___c0731555
     {
         //student name navita navita
         //studentname gagandeep sidhu
-        public delegate void MyDelegate();
-        void Method1(int i)
-        {
-            Console.WriteLine("Method1");
-            Console.ReadLine();
-        }
-        public void Method2()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
+        public delegate void MyDelegate(int intValue);
+        public Method1(int intMethod1)
+            {
+          return intMethod1*2;
+            }  
+         public Method1(int intMethod2)
+            {
+          return intMethod2*10;
+            }
+       
+       
 
     }
 }
